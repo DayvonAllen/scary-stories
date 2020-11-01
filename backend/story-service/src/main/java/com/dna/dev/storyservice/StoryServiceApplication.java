89f607c862cf23becb64c870b2e702c828d0c930
DayvonAllen <dayvonallen93@outlook.com>
2020-10-31 20:21:59 -0700

    first commit

---
+++ backend/story-service/src/main/java/com/dna/dev/storyservice/StoryServiceApplication.java
@@ -0,0 +1,15 @@
+package com.dna.dev.storyservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class StoryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StoryServiceApplication.class, args);
+    }
+
+}
